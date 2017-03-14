410eb323a0bf1aa1d5ec3b4deb0fa5cf81575384
alexandre-mouton-brady <amoutonbrady@gmail.com>
2017-03-14 19:14:10 +0100

    Style de la page d'inscription orientée material design

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Le meilleur moyen de s'impliquer dans une bonne cause. Simple et facile, participer au financement des projets GSB aujourd'hui?.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Starter Kit</title>
+
+    <!-- Your styles -->
+    <link href="./styles/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <header class="header">
+      <section class="header__section">
+        <span class="header__title">GSB - Partenaires</span>
+      </section>
+      <section class="header__section">
+      </section>
+    </header>
+    <main class="content-wrapper">
+      <form action="" autocomplete="nope" method="post" class="form">
+        <h1 class="form__title">Connexion partenaire</h1>
+        <div class="input">
+          <input type="text" class="input__field" name="login" id="login" required value="">
+          <label for="login" class="input__label">Login</label>
+          <div class="input__focus"></div>
+        </div>
+        <div class="input">
+          <input type="password" class="input__field" name="motDePasse" id="motDePasse" required value="">
+          <label for="motDePasse" class="input__label">Mot de passe</label>
+          <div class="input__focus"></div>
+        </div>
+        <div class="group">
+          <button class="btn">
+            <span class="btn__text">Inscription</span>
+          </button>
+          <button class="btn btn--raised">
+            <span class="btn__text">Connexion</span>
+          </button>
+        </div>
+      </form>
+    </main>
+
+    <script src="./scripts/index.js"></script>
+  </body>
+</html>
